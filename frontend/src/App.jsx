228c38e9f64625b70f98735a8ee28efd5630d1d4
TanveerAhmadDev <tanveeera84@gmail.com>
2026-09-16 23:37:06 +0500

    Frontend Minor Error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
-import UserDashboard from "./pages/userDashboard";
+import UserDashboard from "./pages/UserDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 
 import ProtectedRoute from "./ProtectedRoute";
